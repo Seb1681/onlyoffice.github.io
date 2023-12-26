9ce74e039acc9e277e4be85cc6624f9fc0f01752
Sebastian Soon <sebastian@azaas.com>
2023-12-26 12:20:58 +0800

    test: tweak prompt

--- sdkjs-plugins/content/boa_ai/scripts/code.js
+++ sdkjs-plugins/content/boa_ai/scripts/code.js
@@ -250,7 +250,7 @@
     // generate content in document
     window.Asc.plugin.attachContextMenuClickEvent('generate', function () {
         window.Asc.plugin.executeMethod('GetSelectedText', null, (text) => {
-            let prompt = (`Please generate the content of the following prompt. ${text}. For this reply, please reply with the documentation formatted content only.`);
+            let prompt = (`Please generate the content for: "${text}". For this reply, please reply with the documentation formatted content only.`);
             typingIndicator.style.display = 'block'; // display the typing indicator
             sseRequest(prompt)
                 .then(result => {
@@ -276,7 +276,7 @@
     // rephrase content in document
     window.Asc.plugin.attachContextMenuClickEvent('rephrase', function () {
         window.Asc.plugin.executeMethod('GetSelectedText', null, (text) => {
-            let prompt = (`Please rephrase this sentence. ${text}. For this reply, please reply with the documentation formatted content only`);
+            let prompt = (`Please rephrase this sentence: "${text}". For this reply, please reply with the documentation formatted content only`);
             typingIndicator.style.display = 'block'; // display the typing indicator
             sseRequest(prompt)
                 .then(result => {

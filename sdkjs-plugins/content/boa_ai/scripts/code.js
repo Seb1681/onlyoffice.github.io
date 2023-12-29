690276c396d048b82f3212eaa72a5bcb6628cafc
Sebastian Soon <sebastian@azaas.com>
2023-12-29 20:36:27 +0800

    test

--- sdkjs-plugins/content/boa_ai/scripts/code.js
+++ sdkjs-plugins/content/boa_ai/scripts/code.js
@@ -271,20 +271,19 @@
             let prompt = (`Please generate the content for: "${text}". For this reply, please reply with the documentation formatted content only.`);
             typingIndicator.innerHTML = 'Generating...';
             typingIndicator.style.display = 'block'; // display the typing indicator
-
-            fetch("https://admin.dev.boa.azaas.online/api/ai/rsd/ai-prompt", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": "Bearer " + token,
-                    "Cookie": "INGRESSCOOKIE=fa67db442bf86d1c67a2b1c5b5dec6be|f22943aae6fdeef03b5428981d483513"
-                },
-                body: JSON.stringify({
-                    "RsdId": rsdId,
-                    "Question": prompt
-                })
-            })
-            .then(response => console.log(response, 'response'));
+            fetch(
+                "https://ai.azaas.com/api/v1/prediction/97bd8c9a-5f24-4bb2-8484-a0d3a3b8f041",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(
+                        {"question": question},
+                        {"metadata": ""}
+                    )
+                }
+            ).then(response => console.log(response));
 
             sseRequest(prompt)
                 .then(result => {

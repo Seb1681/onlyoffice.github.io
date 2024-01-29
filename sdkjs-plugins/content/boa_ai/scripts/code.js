1fc26f90e4d450f428369a6b4f7e009a1d089b5d
Sebastian Soon <sebastian@azaas.com>
2024-01-30 06:50:35 +0800

    tweak prompt

--- sdkjs-plugins/content/boa_ai/scripts/code.js
+++ sdkjs-plugins/content/boa_ai/scripts/code.js
@@ -115,7 +115,7 @@
     window.Asc.plugin.attachContextMenuClickEvent('generate', function () {
         window.Asc.plugin.executeMethod('GetSelectedText', null, (text) => {
             window.parent.parent.postMessage({"onlyOfficePlugin": "Loading"}, '*');
-            let prompt = (`Please generate the content for: "${text.trim()}". Please reply in markdown format.`);
+            let prompt = (`Please generate the content for: "${text.trim()}". Please reply only the content, in markdown format.`);
             sseRequest(prompt)
                 .then(result => {
                     Asc.scope.p = parseMarkdown(result.text);
@@ -143,7 +143,7 @@
     window.Asc.plugin.attachContextMenuClickEvent('rephrase', function () {
         window.Asc.plugin.executeMethod('GetSelectedText', null, (text) => {
             window.parent.parent.postMessage({"onlyOfficePlugin": "Loading"}, '*');
-            let prompt = (`Please rephrase this sentence: "${text.trim()}". Please reply in markdown format.`);
+            let prompt = (`Please rephrase this sentence: "${text.trim()}". Please reply only the content, in markdown format.`);
             sseRequest(prompt)
                 .then(result => {
                     Asc.scope.p = parseMarkdown(result.text);
